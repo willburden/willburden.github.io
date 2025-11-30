8a8983a84d99ed75d7aa8a2abbba7a6e42f57731
Will Burden <25230174+willburden@users.noreply.github.com>
2025-11-30 11:43:27 +0000

    Optimise hero image loading priority

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,5 @@ export default defineConfig({
     image: {
         responsiveStyles: true,
         layout: 'constrained',
-        objectFit: 'scale-down',
-        objectPosition: 'center',
-        breakpoints: [400, 640, 750, 828, 1080, 1280, 1668, 2048, 2560],
     },
 });
